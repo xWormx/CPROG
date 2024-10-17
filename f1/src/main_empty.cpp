a0293f433c0c55185206d96684537e184e3ddd99
xWormx <carl_johan6@hotmail.com>
2024-10-17 19:53:07 +0200

    f1 & f2 i princip klara

--- f1/src/main_empty.cpp
+++ f1/src/main_empty.cpp
@@ -1,9 +1,52 @@
-//
-//  c++_empty
-//  
 
-#include <iostream>
+
+/*
+
+	Preprocessor direktiv
+	#include-direktiv ersätt med innehållet i filen
+
+	<filnam> - headerfilen sökes i standardkataloger
+	"filnamn" - headerfilen sökes först i aktuell katalog och katalogerna i 
+				"include-sökvägen", i andra han i standardkataloger.
+
+	för att komma åt ett namn i en namnrymd (namespace) skrivs: 
+	[namnrymdens namn] [::] [aktuellt namn]
+	
+	ex: 
+	std::cout << "hej" << std:endl;
+	
+	:: kalla räckvidsoperatorn
+
+	man kan öppna en namnrymd genom att använda using namespace-direktiv
+	ex:
+	using namespace std;
+
+	primitiva typer
+
+	Heltal:
+		char
+		short
+		int
+		long
+		long long
+	
+	Flyttal:
+		float 
+		double
+		long double
+
+	Boolean:
+		bool
+
+	förutom char (som är en byte) är inte storleken för dessa typer definierad av standarden, utan bestäms
+	av datorarkitekturen i aktuell dator.
+	Operatorn sizeof(typ) ger storleken i antal bytes
+*/
+
+
+#include <iostream> 
 #include <string>
+#include "Constants.h"
 
 int main(int argc, char* argv[]) {
 	std::string s1 = "Hejsan";
--- f2/src/main_empty.cpp
+++ f2/src/main_empty.cpp
@@ -1,13 +1,57 @@
-//
-//  c++_empty
-//  
+/*
+
+	Funktioner måste deklareras innan de kan användas.
+	En funktionsdeklaration gör att funktionen kan användas innan dess definition
+
+	Ex:
+
+	int funktion(int a);  <------ Deklaration av funktionen funktion()
+
+	int main(int argc, char **argv)
+	{
+		int a = funktion(3);  <------------ Eftersom funktionen är deklarerad kan den kallas på här, innan definitionen.
+
+		return 0;
+	}
+
+	int funktion(int a)   <--------- Definition
+	{
+		std::cout << a << std::endl;
+
+		return a;
+	}
+
+*/
 
 #include <iostream>
 #include <string>
+#include "cstring"
+
+
+int add(int a, int b);
 
 int main(int argc, char* argv[]) {
-	std::string s1 = "Hejsan";
-	std::string gitTest = "GIT TEST";
-	std::cout << s1 << std::endl << gitTest << std::endl;
+
+	int a = add(2, 3);
+	std::cout << "a is 2 + 3 which is : " << a << std::endl; 
+	
+	char arr[6] = "calle";
+	char copyArr[6];
+
+	printf("%s\n", arr);
+
+	strcpy(copyArr, arr);
+
+	printf("%s has %d characters in it\n", copyArr, strlen(copyArr));
+	
+	for(int i = 0; i < strlen(copyArr); i++)
+	{
+		printf("%d", i+1);
+	}
+
 	return 0;
+}
+int add(int a, int b)
+{
+	return a + b;
 }
