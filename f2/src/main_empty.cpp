9a8e55521c7147d5be8dfd75b5a786b7eeac3741
xWormx <carl_johan6@hotmail.com>
2024-10-17 10:50:00 +0200

    f2 - mall tillagd

---
+++ f2/src/main_empty.cpp
@@ -0,0 +1,13 @@
+//
+//  c++_empty
+//  
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	std::string s1 = "Hejsan";
+	std::string gitTest = "GIT TEST";
+	std::cout << s1 << std::endl << gitTest << std::endl;
+	return 0;
+}
